82debb56888f6ee7479b5a95233d06985e44d7c9
Gina Vargas <gina.vargas89@outlook.com>
2021-02-25 13:56:46 -0800

    youtube function

---
+++ script.js
@@ -0,0 +1,25 @@
+// add youtube api
+// add youtube search bar 
+// add result menu 
+
+// add button id in HTML for youtube search
+var searchEl = document.querySelector('#search-yt');
+
+// function handleSearchFormSubmit(event) {
+//     event.preventDefault();
+// }
+
+function getYoutubeAPI(searchTerm) {
+    fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=AIzaSyAOJ6CvOZGZ_RiV0g5CnQB1SFeQthDvKlo&q=" + searchTerm)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+    .catch(function(err) {
+        console.error(err)
+    });
+};
+
+getYoutubeAPI("bts")
