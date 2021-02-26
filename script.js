44cc2a23f77131c5e6b5a953655f1ad700635a38
Gina Vargas <gina.vargas89@outlook.com>
2021-02-26 11:49:35 -0800

    added youtube api to reseource page

--- script.js
+++ script.js
@@ -3,23 +3,39 @@
 // add result menu 
 
 // add button id in HTML for youtube search
-var searchEl = document.querySelector('#search-yt');
+var searchContentEl = document.querySelector('#search-yt');
 
 // function handleSearchFormSubmit(event) {
 //     event.preventDefault();
 // }
 
 function getYoutubeAPI(searchTerm) {
-    fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=AIzaSyAOJ6CvOZGZ_RiV0g5CnQB1SFeQthDvKlo&q=" + searchTerm)
+    fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=surfing&key=AIzaSyAOJ6CvOZGZ_RiV0g5CnQB1SFeQthDvKlo&q=" + searchTerm + "&order=date")
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         console.log(data)
+        searchContentEl.relatedToVideoId = "Javacript"
+        console.log(data.items[0].id.videoId)
+
+        for (var i=0; i < 3; i++){
+            var videoId = data.items[i].id.videoId
+        $("#video-content").append('<iframe width="560″ height="315″ src="https://www.youtube.com/embed/' + videoId + '" frameborder="0″ allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+        }
+
+        
     })
     .catch(function(err) {
         console.error(err)
     });
 };
 
-getYoutubeAPI("javascript")
+
+
+$("#floatingSelect").on("change" , function(){
+    var selectLanguage = $("#floatingSelect").val()
+    console.log(selectLanguage)
+    $("#video-content").empty()
+    getYoutubeAPI(selectLanguage)
+})
